b6de384885de9c671a3c3fec240bc3d0ea11f135
MinhNghia2k11 <144499974+MinhNghia2k11@users.noreply.github.com>
2024-07-30 23:00:09 -0700

    Update SitinksHub.lua

--- SitinksHub.lua
+++ SitinksHub.lua
@@ -7,6 +7,13 @@
 ╚═════╝  ╚═╝   ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝  ╚═╝  ╚═══╝   ╚═════╝ 
 ]]--
 
+repeat task.wait() until game:IsLoaded()
+Notify = require(game.ReplicatedStorage:WaitForChild("Notification"))
+Notify.new("<Color=Red>Welcome to sitink hub<Color=/>"):Display()
+Notify.new("<Color=Black>Owner : nightx12082k11<Color=/>"):Display() 
+Notify.new("<Color=Red>Discord: https://discord.gg/JURvkuHr28<Color=/>"):Display() 
+Notify.new("<Color=Yellow>Exploit Use : <Color=/> ".. identifyexecutor()):Display() 
+
 local PlaceId = game.PlaceId
 local plr = game.Players.LocalPlayer
 
