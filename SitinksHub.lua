0c862e4fe6de566e01a0bd1da0940afc6a9d51a7
MinhNghia2k11 <144499974+MinhNghia2k11@users.noreply.github.com>
2024-07-31 06:53:17 -0700

    Update SitinksHub.lua

--- SitinksHub.lua
+++ SitinksHub.lua
@@ -12,18 +12,18 @@ Notify = require(game.ReplicatedStorage:WaitForChild("Notification"))
 Notify.new("<Color=Red>Welcome to sitink hub<Color=/>"):Display()
 Notify.new("<Color=Black>Owner : nightx12082k11<Color=/>"):Display() 
 Notify.new("<Color=Red>Discord: https://discord.gg/JURvkuHr28<Color=/>"):Display() 
-Notify.new("<Color=Yellow>Exploit Use : <Color=/> ".. identifyexecutor()):Display() 
+Notify.new("<Color=Yellow>You Executor : <Color=/> ".. identifyexecutor()):Display() 
 
 local PlaceId = game.PlaceId
 local plr = game.Players.LocalPlayer
 
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MinhNghia2k11/Main/main/Main-BF"))()
-    Hookfunction = hookfunction or hookfunc
+ Hookfunction = hookfunction or hookfunc
     local Death = require(game.ReplicatedStorage.Effect.Container:WaitForChild("Death"))
     local Respawn = require(game.ReplicatedStorage.Effect.Container:WaitForChild("Respawn"))
     Hookfunction(Death, function() return nil end)
     Hookfunction(Respawn, function() return nil end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MinhNghia2k11/Main/main/Main-BF"))()
 else
     plr:Kick("Đéo support Cook!!")
 end
